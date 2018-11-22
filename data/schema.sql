fcf06dc80f2c384dd88045dd4c8944c42f430484
Zach Baylin <zbaylin@gmail.com>
2018-11-22 14:01:12 -0500

    updated the publication_date field to be a TEXT field

--- data/schema.sql
+++ data/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE "book" (
+CREATE TABLE "books" (
   "id" INTEGER NOT NULL PRIMARY KEY, 
   "isbn" VARCHAR(255) NOT NULL,
   "title" VARCHAR(255) NOT NULL, 
   "author" VARCHAR(255) NOT NULL, 
   "publisher" VARCHAR(255) NOT NULL, 
   "summary" TEXT NOT NULL, 
-  "publication_date" DATE NOT NULL
+  "publication_date" TEXT NOT NULL
 );
