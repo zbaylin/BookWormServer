8bd805659f5cb759b3ae6b145e0a1c4be1c1be41
Zach Baylin <zbaylin@gmail.com>
2018-11-22 11:53:30 -0500

    added a base schema for books

---
+++ data/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "book" (
+  "id" INTEGER NOT NULL PRIMARY KEY, 
+  "isbn" VARCHAR(255) NOT NULL,
+  "title" VARCHAR(255) NOT NULL, 
+  "author" VARCHAR(255) NOT NULL, 
+  "publisher" VARCHAR(255) NOT NULL, 
+  "summary" TEXT NOT NULL, 
+  "publication_date" DATE NOT NULL
+);
