7ef41454676924d77660e275a8e51ad2c7e4ce72
Zach Baylin <zbaylin@gmail.com>
2018-11-22 14:28:45 -0500

    added error catching to the route(s)

--- src/routes/api/books.cr
+++ src/routes/api/books.cr
@@ -2,7 +2,12 @@
 
 module BookWormServer
   get "/api/books" do |env|
-    books = Repo.all(Book)
-    books.to_json
+    begin
+      books = Repo.all(Book)
+      {"success": true, "books": books}.to_json
+    rescue exception
+      response = {"success": false, "message": "Unable to retrieve books: #{exception}"}.to_json
+      halt env, status_code: 500, response: response
+    end
   end
 end
