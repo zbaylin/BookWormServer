42a9a224fe1ce21cd3f5894859fab0f91580e118
Zach Baylin <zbaylin@gmail.com>
2018-11-22 14:31:57 -0500

    added comments to the books API route

--- src/routes/api/books.cr
+++ src/routes/api/books.cr
@@ -3,10 +3,14 @@
 module BookWormServer
   get "/api/books" do |env|
     begin
+      # Get all books from the database
       books = Repo.all(Book)
+      # Return a successful request with the books in JSON
       {"success": true, "books": books}.to_json
     rescue exception
+      # Prepare a response with the error
       response = {"success": false, "message": "Unable to retrieve books: #{exception}"}.to_json
+      # Halt the environment with an error code and send the response
       halt env, status_code: 500, response: response
     end
   end
